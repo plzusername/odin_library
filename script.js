7b9357bc9abc4cc4ff1d89f240f8fe9f5dfed4e2
plzusername <MohamedFaris2210@gmail.com>
2023-11-01 18:54:31 +0300

    define card items when book constructor is called

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 let library=[]
+
 function Book(title,author,pages,read){
     this.title=title
     this.author=author
     this.pages=pages
     this.read=read
     this.id=library.length
+    this.makeBook=function(){
+        let card=document.createElement('div')
+        let title=document.createElement('h2')
+        let authors=document.createElement('span')
+        let pages=document.createElement('p')
+        let iconCon=document.createElement('div')
+        let readCon=document.createElement('div')
+        let read=document.createElement('input');read.setAttribute("type", "checkbox");
+        let label=document.createElement('label')
+        let edit=document.createElement('i');edit.setAttribute('class','fa-pen-to-square fa-solid')
+        let remove=document.createElement('i');remove.setAttribute('class','fa-solid fa-trash')
+    }
 }
